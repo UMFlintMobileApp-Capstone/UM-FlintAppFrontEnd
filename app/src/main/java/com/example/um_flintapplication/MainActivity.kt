3c00fcc21d2a52d2f02a0e012014b0dff84b92de
sald3 <saldim@umich.edu>
2024-11-04 16:53:37 -0500

    Updates to the Home page and nav

--- app/src/main/java/com/example/um_flintapplication/MainActivity.kt
+++ app/src/main/java/com/example/um_flintapplication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.um_flintapplication
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -25,17 +24,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.appBarMain.toolbar)
+        binding.appBarMain.toolbar.title = "University of Michigan - Flint" // Set the toolbar title here
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        // Define top-level destinations for the navigation
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
