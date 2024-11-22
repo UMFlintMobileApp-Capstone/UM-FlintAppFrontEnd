038b64a9527cc1d5f5c0d247862ffc98cf19c953
CalebLong <99842814+ITI100CalebL@users.noreply.github.com>
2024-11-21 22:29:15 -0500

    Added navigation to DepartmentInformationActivity.kt and AcademicCalendar.kt

--- app/src/main/java/com/example/um_flintapplication/DepartmentInformationActivity.kt
+++ app/src/main/java/com/example/um_flintapplication/DepartmentInformationActivity.kt
@@ -3,6 +3,7 @@ package com.example.um_flintapplication
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -37,27 +38,10 @@ class DepartmentInformationActivity : AppCompatActivity() {
         binding = ActivityDepartmentInformationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.appBarDepartmentInformation.toolbar)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = "Academic Calendar"
 
-        binding.appBarDepartmentInformation.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_department_information)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-        // Start the scraping task
-        fetchDepartmentData()
+        setupNavigationDrawer()
     }
 
     private fun setupNavigationDrawer() {
@@ -83,12 +67,12 @@ class DepartmentInformationActivity : AppCompatActivity() {
                     startActivity(intent)
                     true
                 }
-//                R.id.nav_resources_academic_calendar -> {
-//                    // Navigate to Academic Calendar page
-//                    val intent = Intent(this, AcademicCalendarActivity::class.java)
-//                    startActivity(intent)
-//                    true
-//                }
+                R.id.nav_resources_academic_calendar -> {
+                    // Navigate to Academic Calendar page
+                    val intent = Intent(this, AcademicCalendar::class.java)
+                    startActivity(intent)
+                    true
+                }
                 R.id.nav_resources_departments -> {
                     // Navigate to Departments page
                     val intent = Intent(this, DepartmentInformationActivity::class.java)
@@ -165,14 +149,10 @@ class DepartmentInformationActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.department_information, menu)
-        return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_department_information)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
